2e3e765264ddbec63927b42754a894ae96919e18
RatatornAmornwatee <ratatorn.am@gmail.com>
2025-10-02 18:42:08 +0700

    Update Lab 1.1

---
+++ Lab 1.1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+  int num ;
+  int result = 0;
+
+  scanf( "%d" , &num ) ;
+  result += num ;
+
+  scanf( "%d" , &num ) ;
+  result += num ;
+
+  scanf( "%d" , &num ) ;
+  result += num ;
+
+  printf( "%d" , result ) ;
+}//end function
